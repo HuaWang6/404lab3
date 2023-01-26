59eff5cc194942a94beab9e5581c2af236db8db4
hua wang <hua6@ualberta.ca>
2023-01-25 20:42:32 -0500

    Initial commit

--- secret.py
+++ secret.py
@@ -12,8 +12,8 @@ class FollowingTheTAsInstructionsError(Exception):
         ))
 
 # Delete this line:
-raise FollowingTheTAsInstructionsError
+#raise FollowingTheTAsInstructionsError
 
 # Edit the following two lines:
-username = "<pick a username here>"
-password = "<pick a password here>"
+username = "hua6"
+password = "wh1564443949"
